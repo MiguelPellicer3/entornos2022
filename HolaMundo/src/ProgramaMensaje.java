41774090a21c03b1557fe5fe5922e365f4fedcf1
MiguelB01 <63378@iessierradeguara.com>
2022-02-04 11:51:02 +0100

    atajos

--- HolaMundo/src/ProgramaMensaje.java
+++ HolaMundo/src/ProgramaMensaje.java
@@ -5,6 +5,10 @@ public class ProgramaMensaje {
 	public static void main(String[] args) {
 		Mensaje m = new Mensaje();
 		m.setEmisor("Pepito");
-		System.out.println(m);
+		for (int i = 0; i < args.length; i++) {
+			if (i%2==0) {
+				System.out.println(m);
+			}
+		}	
 	}
 }
